e3f507556e3857b0ac86b923acd7d9a8ac77fe22
liujianwei <liujianwei@zuoyebang.com>
2019-05-30 10:40:07 +0800

    require-directory 自动加载路由

--- example/main.js
+++ example/main.js
@@ -1,20 +1,32 @@
 /**
- * 多Router拆分路由
- * api都是有版本的
- * v1,v2,v3支持三个版本
- * 开闭原则：对修改关闭，对拓展开放
- * 版本号控制：
- * 1.路径里面            e.g '/v1/get/userinfo'
- * 2.查询字符串里面       e.g '/get/userinfo?version=v1'
- * 3.header             放到http header里面
+ * require-directory 实现路由自动加载
+ * //相对于 入口文件的所有的 模块
+ * - const modules=requireDirectory(module) 
+ * //会返回所有模块 包括不是路由的模块
+ * - const modules = requireDirectory(module, './api/v1') 
+ * //在visit 做过滤，只返回路由模块 .路由模块 导出的是module.exports=router .是Router的一个实例，所以可以通过判断 item instanceof Router 判断是不是一个路由
+ * - const modules=requireDirectory(module,'./api/v1',{
+ *      visit:()=>{
+ *        if (item instanceof Router) {
+             app.use(item.routes())
+          }
+ *     }
+ * })
  */
 const Koa = require('koa')
-const book = require('./api/v1/book')
-const classic = require('./api/v1/classic')
+const Router = require('koa-router')
+const requireDirectory = require('require-directory')
 
 const app = new Koa()
-app.use(book.routes())
-app.use(classic.routes())
+// const modules=requireDirectory(module) 
+// const modules = requireDirectory(module, './api/v1') //
+const modules = requireDirectory(module, './api/v1', {
+  visit: (item) => {
+    if (item instanceof Router) {
+      app.use(item.routes())
+    }
+  }
+})
 
 app.listen(3000)
 
